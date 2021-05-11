033a82648cf3093aad98638fc19e27eb493c93e6
MarianaVinevala <72925205+MarianaVinevala@users.noreply.github.com>
2021-05-11 21:26:31 +0100

    Add files via upload

---
+++ main/java/com/example/exercicio12/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.exercicio12;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+
+public class MainActivity extends AppCompatActivity {
+    Button btn;
+    EditText user, pass;
+    TextView txt;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main);
+            btn = findViewById(R.id.btn);
+            user = findViewById(R.id.user);
+            pass = findViewById(R.id.pass);
+            txt = findViewById(R.id.txt);
+        }
+
+        public void Login(View view)
+    {
+
+            String username=user.getText().toString();
+            String password =pass.getText().toString();
+            if ((password.equals("pass")) && ((username.equals("User")) || (username.equals("USER")) || (username.equals("user"))))
+            {
+                txt.setText("Login Válido");
+                user.setText("");
+                pass.setText("");
+            } else {
+                txt.setText("Login Inválido");
+                user.setText("");
+                pass.setText("");
+            }
+        }
+
+    }
